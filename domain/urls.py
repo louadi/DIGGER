6a3929c476fa251db1a453bf97c55670fa3c252d
zakaria louadi <zakaria.louadi@gmail.com>
2020-05-11 00:24:14 +0200

    fix-issue-when-interactions-between-only-2-isoforms
    1- fix-issue-when-interactions-between-only-2-isoforms
    2- Add example to the network file

--- domain/urls.py
+++ domain/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('vis_network/job/<str:job>', views.Multi_proteins, name="Network_vis"),
     path('Network_analysis/example', views.example1, name="Network_example1"),
     path('Network_analysis/example2', views.example2, name="Network_example2"),
+    path('Network_analysis/example3', views.example3, name="Network_example3"),
     path('about/', views.about, name="about_page"),
     path('graph/', views.graph, name="graph"),
     path('graph/<str:Pfam_id>', views.display, name="Node_vis"),
