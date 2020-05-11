2fe134d8b939faa2695ca9fe6203dde4bb675e86
Kevin Yuan <kevihiiin@users.noreply.github.com>
2020-05-11 23:00:12 +0200

    Fix BASE_DIR due to moved settings

--- django_project/settings/common.py
+++ django_project/settings/common.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from sqlalchemy import create_engine
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
