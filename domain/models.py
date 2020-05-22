0a36e5443c68f45a33e32f33407b8962927caa59
Kevin Yuan <kevihiiin@users.noreply.github.com>
2020-05-22 21:32:39 +0200

    Import gene mapping table from file

--- domain/models.py
+++ domain/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Gene(models.Model):
     ensembl_id = models.CharField(max_length=20)
-    gene_symbol = models.CharField(max_length=20)
+    gene_symbol = models.CharField(max_length=20, db_index=True)
 
     def __str__(self):
         return self.gene_symbol
