335eb952bedae4825f0d2b3f5a7a3c3f1e07559c
Kevin Yuan <kevihiiin@users.noreply.github.com>
2020-05-24 01:01:09 +0200

    Remove pybiomart dependency

--- domain/database/generate_files.py
+++ domain/database/generate_files.py
@@ -1,16 +1,16 @@
 # from domain.models import Gene
-from pprint import pprint
-
-from pybiomart import Dataset
-
-dataset = Dataset(name='hsapiens_gene_ensembl',
-                  host='http://www.ensembl.org')
-
-mapping_df = dataset.query(attributes=['ensembl_gene_id', 'external_gene_name'],
-                           filters={'chromosome_name': list(range(1,23)) + ['X', 'Y']})
-
-print(mapping_df)
-pprint(dataset.filters)
+# from pprint import pprint
+#
+# from pybiomart import Dataset
+#
+# dataset = Dataset(name='hsapiens_gene_ensembl',
+#                   host='http://www.ensembl.org')
+#
+# mapping_df = dataset.query(attributes=['ensembl_gene_id', 'external_gene_name'],
+#                            filters={'chromosome_name': list(range(1,23)) + ['X', 'Y']})
+#
+# print(mapping_df)
+# pprint(dataset.filters)
 
 # for _, row in mapping_df.iterrows():
 #     gene = Gene()
