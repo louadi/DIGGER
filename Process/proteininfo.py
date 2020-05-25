eba4d2edfe7463585c03e444ca96529cfdfb3122
zakaria louadi <zakaria.louadi@gmail.com>
2020-05-25 14:39:44 +0200

    minor changes

---
+++ Process/proteininfo.py
@@ -0,0 +1,109 @@
+from domain.Process import process_data as pr
+from domain.Process import exonstodomain as exd 
+
+
+import os
+from django.conf import settings
+from dna_features_viewer import GraphicFeature, GraphicRecord, CircularGraphicRecord
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('agg')
+
+# Global image path
+images_path = os.path.join(settings.MEDIA_ROOT, 'images/')
+if not os.path.exists(images_path):
+    os.makedirs(images_path)
+
+def get_protein_info(ID):
+
+  #Get data about the input transcript or the protein 
+  info,trID=ID_mapper(ID) 
+  if info==0: return 0
+  domains,unique,exons,text1,domainshtml,Text_nodes,text_edges,tran_name,gene_name,Ensemble_geneID,entrezID,gene_description,exons,droped1,droped2,exons_in_interface=info
+  #save Image of protein Structure
+
+  Protein_structure(trID,exons,domains,images_path,trID,exons_in_interface)
+
+  return domains,unique,exons,text1,domainshtml,Text_nodes,text_edges,tran_name,gene_name,Ensemble_geneID,entrezID,gene_description,exons,droped1,droped2,trID,images_path
+
+def get_protein_info2(ID):
+
+  #Get data about the input transcript or the protein 
+  info,trID=ID_mapper(ID) 
+  if info==0: return 0
+  domains,unique,exons,text1,domainshtml,Text_nodes,text_edges,tran_name,gene_name,Ensemble_geneID,entrezID,gene_description,exons,droped1,droped2,exons_in_interface=info
+
+
+  return domains,unique,exons,text1,domainshtml,Text_nodes,text_edges,tran_name,gene_name,Ensemble_geneID,entrezID,gene_description,exons,droped1,droped2,trID
+
+
+
+def ID_mapper(ID):
+    # The use input ID of a transcript or protein:
+    
+    #if the ID is a transcript ID:
+    if ID[3]=="T" :return exd.input_transcript(ID),ID
+
+    #if the ID is a protein ID:
+    trID=pr.protein_to_transcript(ID)
+    if ID[3]=="P" :return exd.input_transcript(trID),trID
+
+
+
+
+
+def Visualize_transciript(exon_table,domain_table,exons_in_interface):
+    features1=[]
+    features2=[]
+    for st,e,rank,idd in zip(exon_table["CDS start"],exon_table["CDS end"],exon_table["Exon rank in transcript"],exon_table["Exon stable ID"]):
+        if not np.isnan(st):
+            if idd not in exons_in_interface:
+                features1.append(
+                    GraphicFeature(ax=1,start=st/3, end=e/3, color="#ffd700",label=str(rank)))
+                fend=e/3
+            else: 
+                features1.append(
+                    GraphicFeature(ax=1,start=st/3, end=e/3, color="#FF9200",label=str(rank)))
+                fend=e/3
+    domain_table=domain_table[["Pfam ID","Pfam start","Pfam end","Pfam known interactions"]]
+    domain_table=domain_table.drop_duplicates()
+    for st,e,i in zip(domain_table["Pfam start"],domain_table["Pfam end"],domain_table["Pfam ID"]):
+        if not np.isnan(st):
+            features2.append(
+                GraphicFeature(ax=2,start=st, end=e, color="#ffcccc",label=i))
+    return features1,features2,fend
+
+
+
+
+
+
+
+
+
+
+
+def Protein_structure(ID,exons,domains,path,trID,exons_in_interface):
+    #save Image of protein Structure
+    features1,features2,fend=Visualize_transciript(exons,domains,exons_in_interface)
+    
+    fig, (ax1, ax2) = plt.subplots(
+        2, 1,figsize=(16, 3.5))
+    
+    record = GraphicRecord(sequence_length=fend, features=features1,)
+    record.plot(ax=ax1,figure_width=23,with_ruler=False)
+    
+    record = GraphicRecord(sequence_length=fend, features=features2,)
+    record.plot(ax=ax2,figure_width=23,with_ruler=True,annotate_inline=True)
+    
+    ax1.title.set_text('Coding Exons')
+    ax1.title.set_position([.5, -0.4])
+    ax2.title.set_text('Pfam Domains')
+    ax2.title.set_position([.5, -0.5])
+    
+      
+    fig.savefig(path+trID, bbox_inches='tight')
+    return 
+
+
---
+++ Process/transcript.py
@@ -0,0 +1,422 @@
+import os
+
+from domain.Process import process_data as pr
+from domain.Process import exonstodomain as exd 
+from domain.Process import proteininfo as  info
+import pandas as pd
+from django.urls import reverse
+
+
+from sqlalchemy import text
+
+from django.conf import settings
+# --- Get database connection aka 'SQLAlchemie engine'
+engine = settings.DATABASE_ENGINE
+
+# --- Create folder
+# Global table path
+table_path_2 = os.path.join(settings.MEDIA_ROOT, 'table 2')
+if not os.path.exists(table_path_2):
+    os.makedirs(table_path_2)
+
+PPI=exd.load_obj("PPI")
+g2d=exd.load_obj("g2d")
+DomainG=exd.load_obj("DomainG")
+
+
+
+
+def Protein_view(P_id):
+    
+    i=info.get_protein_info(P_id)
+    if i==0: return 0
+    domains,unique_domains,exons,text1,domainshtml,Text_nodes,text_edges,tran_name,gene_name,Ensemble_geneID,entrezID,gene_description,exons,droped1,droped2,trID,p=i
+    
+
+    
+        
+    if PPI.has_node(entrezID):
+        g = exd.nx.Graph()
+        g.add_edges_from(PPI.edges(entrezID))
+        
+    else : 
+          print('no interactions')
+          return 1
+        
+    #search for the protein domains interactions:
+    # before coming here need to check if the protein have known domains
+    protein_domain=g2d[entrezID]
+    missing_domain=[]
+    
+    for domain in protein_domain:
+        node=entrezID+'/'+domain
+        
+        #check for missing domains in the transcript (for visualization):
+        if domain not in unique_domains:
+
+            missing_domain.append(node)
+
+        #add domain interactions to the graph:       
+        if DomainG.has_node(node):
+            g.add_edges_from(DomainG.edges(node))
+    
+    edges=[]
+    #list contains protein confirmed by both PPI and DDI (for visualization)
+    protein_with_DDI=[]
+    
+    #link domains with their proteins
+    for node in g.nodes():
+            #a node is domain node:
+            if len(node.split('/'))==2 :
+                edges.append((node.split('/')[0],node))
+                protein_with_DDI.append((node.split('/')[0]))
+                
+    g.add_edges_from(edges)
+    
+    
+    protein_with_DDI = list(set(protein_with_DDI))
+    nodes,edges,_=vis_node_(g,entrezID,protein_with_DDI,tran_name,missing_domain)
+    
+    df_missed=[]
+    if missing_domain!=[]:
+            n=[]
+            l=[]
+            dom=[]
+            h=reverse('home')+"graph/"
+            for d in missing_domain:
+                s=d.split('/')
+                dom.append(s[1])
+                if DomainG.has_node(d):
+                    n.append(len(DomainG[d]))
+                    
+                    
+                    l.append('<a target="'+'_blank" href="'+h+entrezID+"."+s[1]+'">'+gene_name+'-'+s[1]+'</a>')
+                else: 
+                    n.append(0)
+                    l.append('')
+                    
+                  
+                  
+            dfff = pd.DataFrame(list(zip(dom,n,l)), columns=['Pfam ID','Pfam known interactions','Visualization of the domain interactions'])
+            pd.set_option('display.max_colwidth',1000)
+            
+            
+            
+            dfff["Pfam known interactions"]='<center>'+dfff["Pfam known interactions"].astype(str)+'</center>'
+            dfff["Visualization of the domain interactions"]='<center>'+dfff["Visualization of the domain interactions"]+'</center>'
+            df_missed=dfff.to_html(escape=False, index=False)
+    
+            
+    
+    
+    
+    #interactionView:
+    
+    if len(protein_with_DDI)>1:
+          pd_interaction=table_interaction(tran_name,trID,entrezID,g,protein_with_DDI,missing_domain)
+              
+              
+          pd_interaction.insert(0,'Selected Protein variant','')
+          pd_interaction["Selected Protein variant"]=tran_name
+          pd_interaction.to_csv(f'{table_path_2}/{trID}.csv', index=False,)
+          
+    
+          pd_interaction["Retained DDIs"]='<center>&emsp;'+pd_interaction["Retained DDIs"]+'&emsp;</center>'
+          pd_interaction["Lost DDIs"]='<center>&emsp;'+pd_interaction["Lost DDIs"]+'&emsp;</center>'
+          
+          
+          
+          pd_interaction=pd_interaction.sort_values(by=['Percentage of lost domain-domain interactions'])
+          h=reverse('home')+"ID/"+trID+'/InteractionView/'
+          
+          pd_interaction["Percentage of lost domain-domain interactions"]='<center>'+pd_interaction["Percentage of lost domain-domain interactions"].astype(int).astype(str)+' % '+'</center>'
+          
+          pd_interaction["Protein-protein interaction"]='<center>'+pd_interaction["Protein-protein interaction"]+'<a target="'+'_blank"href="'+h+pd_interaction["NCBI gene ID"]+'">'+" (Visualize) "+'</a>'+'</center>'
+          
+          
+          #dont move it from here
+          pd_interaction["NCBI gene ID"]='<center>'+pd_interaction["NCBI gene ID"]+'</center>'
+          pd_interaction["Selected Protein variant"]='<center>'+pd_interaction["Selected Protein variant"]+'</center>'
+          pd_interaction["Protein name"]='<center>'+pd_interaction["Protein name"]+'</center>'
+          
+          
+          pd_interaction=pd_interaction.rename(columns={
+          "Selected Protein variant": "<center>Selected Protein variant</center>",
+          "Protein name": "<center>Partner Protein </center>", 
+          "NCBI gene ID": "<center>NCBI gene ID</center>", 
+          "Percentage of lost domain-domain interactions": "<center> % of missing DDIs</center>",
+          "Retained DDIs": "<center>&emsp;Retained Domain-Domain interactions</center>", 
+          "Lost DDIs": "<center>Missing Domain-Domain interactions</center>",
+          "Protein-protein interaction": "<center>Protein-protein interaction</center>"
+          })
+          
+          pd.set_option('display.max_colwidth',1000)
+          pd_interaction=pd_interaction.to_html(escape=False, index=False)
+          
+    else: pd_interaction=[]
+    
+    
+    
+    
+    #get a list of all Isoforms of the selected transcript
+    gene_ID=pr.tranID_convert(trID)[2]
+    transcripts=pr.gene_to_all_transcripts(gene_ID)
+    
+    if len(transcripts)>=1:
+  
+          ID=[]
+          name=[]
+          pfams=[]
+          
+          for tr in transcripts :
+                
+              query = """
+              SELECT * 
+              FROM exons_to_domains_data 
+              WHERE "Transcript stable ID"=:transcript_id 
+              """
+              tdata = pd.read_sql_query(sql=text(query), con=engine, params={'transcript_id': tr})
+              
+              # tdata=tdata.drop(columns=["Unnamed: 0"]).drop_duplicates()
+              
+              #df_filter = pr.data['Transcript stable ID'].isin([tr])
+              #tdata=pr.data[df_filter]
+              
+              if len(tdata)!=0  :
+                  ID.append(tr)
+                  name.append(pr.tranID_convert(tr)[0])
+                  
+                  p=tdata["Pfam ID"].unique()
+                  p = p[~pd.isnull(p)]
+                  
+                  pfams.append('&emsp;<center>'+' ; '.join(p)+'</center>&emsp;')
+          
+          
+          if ID!=[]:        
+            pd_isoforms=pd.DataFrame(list(zip(name, ID,pfams)), columns =['<center>Transcript name<center>', '<center>Transcript ID</center>','<center>Pfam domains<center>'])
+            pd_isoforms['length'] = pd_isoforms['<center>Pfam domains<center>'].str.len()
+            pd_isoforms.sort_values('length', ascending=False, inplace=True)
+            pd_isoforms=pd_isoforms.drop(columns=['length'])
+            
+            h=reverse('home')+"ID/"
+            pd_isoforms["<center>Link</center>"]='<center>&emsp;'+'<a target="'+'_blank"href="'+h+pd_isoforms["<center>Transcript ID</center>"]+'">'+" (Visualize) "+'</a>'+'&emsp;</center>'
+            pd_isoforms["<center>Transcript ID</center>"]='<center>&emsp;'+pd_isoforms["<center>Transcript ID</center>"]+'&emsp;</center>'
+            
+            
+            
+            pd_isoforms=pd_isoforms.to_html(escape=False, index=False)
+            
+    else: pd_isoforms=[]
+    
+    
+    
+    
+    return nodes,edges,_,domains,unique_domains,exons,text1,domainshtml,Text_nodes,text_edges,tran_name,gene_name,Ensemble_geneID,entrezID,gene_description,exons,droped1,droped2,trID,p,df_missed,pd_interaction,pd_isoforms
+
+
+
+
+
+
+
+
+
+def Interacted_domain(p,g,entrezID,missing_domain):
+        #Search for interacted domains
+        edges=[]
+        DDI_edges=[]
+        lost_edges=[]
+        for e in g.edges():
+            gene1=e[0].split('/')[0]
+            gene2=e[1].split('/')[0]
+            if p in [gene1,gene2]:
+                edges.append(e)
+                if edge_dashes(e,entrezID,missing_domain)[0]!='false':
+                    lost_edges.append(e[0].split('/')[1]+'-'+e[1].split('/')[1])
+
+                elif len(e[0].split('/'))==2 and len(e[1].split('/'))==2: 
+                    DDI_edges.append(e[0].split('/')[1]+'-'+e[1].split('/')[1])
+        
+        return edges,DDI_edges,lost_edges     
+
+def table_interaction(tran_name,trID,entrezID,g,protein_with_DDI,missing_domain):
+    Interactions=[]
+    IDs=[]
+    DDIs=[]
+    lost_DDIs=[]
+    perc=[]
+    status=[]
+    for protein in protein_with_DDI:
+        #select first protein
+        if protein !=entrezID:
+            #Search for interacted domains
+            edges,DDI_edges,lost_edges=Interacted_domain(protein,g,entrezID,missing_domain)
+            
+            IDs.append(protein)
+            p=len(lost_edges)/(len(DDI_edges)+len(lost_edges))
+            p=float("{0:.2f}".format(p))
+            perc.append(p*100)
+            
+            st='Affected'
+            if p==0: st='Retained'
+            if p==1: st='Missing'
+            
+            status.append(st)
+            
+            Interactions.append(pr.entrez_to_name(protein))
+            DDIs.append(' ; '.join(DDI_edges))
+            lost_DDIs.append(' ; '.join(lost_edges))
+    return  pd.DataFrame(list(zip(Interactions, IDs,DDIs,lost_DDIs,perc,status)), columns =['Protein name', 'NCBI gene ID','Retained DDIs','Lost DDIs','Percentage of lost domain-domain interactions','Protein-protein interaction']) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def vis_node_(g,entrezID,protein_with_DDI,tran_name,missing_domain):
+
+    N=[]
+    E=[]
+    for node in g.nodes():
+        #node of a protein:
+        if node!=entrezID and len(node.split('/'))==1:
+                try:
+                    label=pr.entrez_to_name(node)
+                    N.append("{id: \""+node+"\", label:  \""+label+"\", group:  \""+group_node(node,entrezID)+"\", physics:"+physics(node,entrezID)+
+                             ", source:  \""+source_node(node,entrezID,protein_with_DDI)+"\", value:  \""+value_node(node,entrezID)+"\"},")
+                except KeyError:
+                    print(' ')
+
+                    
+        else:
+                    label=node_label(node,entrezID,tran_name)
+                    N.append("{id: \""+node+"\", label:  \""+label+"\", group:  \""+group_node(node,entrezID)+"\", physics:"+physics(node,entrezID)+
+                             ", source:  \""+source_node(node,entrezID,protein_with_DDI)+"\", value:  \""+value_node(node,entrezID)+"\"},")
+
+    for e in g.edges():
+         E.append("{from: \""+e[0]+"\", to: \""+e[1]+"\", dashes:  "+edge_dashes(e,entrezID,missing_domain)[0]+","+edge_option(e,entrezID)+"},")      
+
+    return N,E,len(g)-1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def edge_option(edge,entrezID):
+    e1=edge[0].split('/')
+    e2=edge[1].split('/')
+    #protein to protein
+    if len(e1)==1 and len(e2)==1:
+        option='length: PR_LENGTH, width: WIDTH_SCALE * 4'
+    #domain to domain
+    elif len(e1)==2 and len(e2)==2:
+        option='length: PR_DM, color: GREEN, width: WIDTH_SCALE * 2'
+        
+    #domain to protein    
+    else    :
+        option='length: LENGTH_domain, color: YELLOW, width: WIDTH_SCALE * 2'
+    return option    
+
+def edge_dashes(edge,entrezID,missing_domain):
+    v="false"
+    lost_int=[]
+    #one of the nodes are missing in this transcript:
+    if any(x in missing_domain for x in edge):
+      
+        v='[2, 2, 10, 10] '
+        lost_int.append(edge)
+    return v,lost_int
+
+
+
+
+
+
+
+
+
+
+# node group
+def group_node(node,entrezID):
+    group="protein"
+    if len(node.split('/'))==2 : 
+        group="domain"
+    
+    return group
+
+# Seperate node by their source: DDI or PPI
+# For visualization
+# all domains node will be in mode 2 in additon to proteins with interacted domains
+def source_node(node,entrezID,protein_with_DDI):
+    source="PPI"
+    if len(node.split('/'))==2 : 
+        source="DDI"
+    elif node in protein_with_DDI :
+        source="DDI"
+    return source
+
+
+def physics(node,entrezID):
+    p='true'
+    if node==entrezID: p='false'
+    return p
+
+
+def value_node(node,entrezID,):
+    v='2'
+    if group_node(node,entrezID)!="domain"  :v='4'
+    return v
+
+def node_label(node,entrezID,tran_name):
+    
+    #node of selected transcript
+    if node==entrezID:  label=tran_name
+        
+    #node is a domain
+    elif  len(node.split('/'))==2 :
+        label=node.split('/')[1]
+    
+    else :
+        label=node
+    return label
