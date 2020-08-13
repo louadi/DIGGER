01279f97c1f8fe007dfde3b9530e1125dfdac500
Kevin Yuan <kevihiiin@users.noreply.github.com>
2020-08-14 01:46:16 +0200

    Reformat function according to PEP standards

--- domain/views.py
+++ domain/views.py
@@ -476,29 +476,23 @@ def exon_level(request):
 
 #PPI network analysis
 def network(request):
-
     # Option 1: List of Ensembl IDs
     if "option1" in request.POST:
-          input_query = []
-          for element in request.POST['input'].split('\n'):
-              element = element.strip()
-              if element:
-                  input_query.append(element)
+        input_query = []
+        for element in request.POST['input'].split('\n'):
+            element = element.strip()
+            if element:
+                input_query.append(element)
 
-          input_query=list(set(input_query))
+        input_query = list(set(input_query))
 
-          #max input IDs
-          if len(input_query)<2000 and len(input_query)>1:
-                if input_query[0][0:4]=='ENSG' or input_query[0][0:4]=='ENST' or input_query[0][0:4]=='ENSP':
-                      job_num=str(random.randrange(500))
-                      with open(f'{jobs_path}/{job_num}.txt', "wb") as fp:   #Pickling
-                             pickle.dump(input_query, fp)
-                      return redirect(Multi_proteins,job=job_num)
-
-
-
-
-
+        # max input IDs
+        if len(input_query) < 2000 and len(input_query) > 1:
+            if input_query[0][0:4] == 'ENSG' or input_query[0][0:4] == 'ENST' or input_query[0][0:4] == 'ENSP':
+                job_num = str(random.randrange(500))
+                with open(f'{jobs_path}/{job_num}.txt', "wb") as fp:  # Pickling
+                    pickle.dump(input_query, fp)
+                return redirect(Multi_proteins, job=job_num)
 
     # Option 2: Upload file
     if "option2" in request.POST and 'gene-count-file' in request.FILES:
@@ -509,13 +503,12 @@ def network(request):
             # Parse as pandas dataframe
             transcript_count_file = pd.read_table(file_buffer)
 
-            #check if the file is alright:
-            if len(transcript_count_file)<2  or len(transcript_count_file)<2 :
-
-                #try to read it as an csv
+            # check if the file is alright:
+            if len(transcript_count_file) < 2 or len(transcript_count_file) < 2:
+                # try to read it as an csv
                 transcript_count_file = pd.read_csv(file_buffer)
 
-            if len(transcript_count_file)<2  or len(transcript_count_file)<2 :
+            if len(transcript_count_file) < 2 or len(transcript_count_file) < 2:
                 return HttpResponse("<h1>File format not supported</h1>")
 
             else:
@@ -523,63 +516,55 @@ def network(request):
                 # TODO Zakaria please insert the magic down below:)
                 # Zaka: And this is where the magic happens :p
 
-                #check if the first row corresponds to transcript Ensembl IDs
+                # check if the first row corresponds to transcript Ensembl IDs
 
-                #Max: the max number of isoforms to consider:
-                Max=1000
+                # Max: the max number of isoforms to consider:
+                Max = 1000
 
-                frist_row=transcript_count_file.iloc[:,0].unique()[:Max]
+                frist_row = transcript_count_file.iloc[:, 0].unique()[:Max]
 
-                if frist_row[0][0:4]!='ENST' or frist_row[1][0:4]!='ENST' :
+                if frist_row[0][0:4] != 'ENST' or frist_row[1][0:4] != 'ENST':
                     return HttpResponse("<h1>File format not supported</h1>")
 
+                # at this point we have at least two transcript :)
+                frist_row = transcript_count_file.iloc[:, 0].unique()[:Max]
 
-                #at this point we have at least two transcript :)
-                frist_row=transcript_count_file.iloc[:,0].unique()[:Max]
-
-                #then check if the column of  counts exist  (for cufflinks FPKM  column)
-                 # if FPKM column exists  >>>> take the most expressed transcripts
+                # then check if the column of  counts exist  (for cufflinks FPKM  column)
+                # if FPKM column exists  >>>> take the most expressed transcripts
                 try:
-                        transcript_count_file=transcript_count_file.sort_values(by=['FPKM'], ascending=False)
-                        frist_row=transcript_count_file.iloc[:,0].unique()[:Max]
-                        print('Input matches cufflinks output ')
+                    transcript_count_file = transcript_count_file.sort_values(by=['FPKM'], ascending=False)
+                    frist_row = transcript_count_file.iloc[:, 0].unique()[:Max]
+                    print('Input matches cufflinks output ')
 
                 except:
                     pass
 
-
-
-
-              #cufflinks file (or a similar thing)
-              #kallisto output counts in tpm
+                # cufflinks file (or a similar thing)
+                # kallisto output counts in tpm
                 try:
-                    #try to find column of counts
-                    transcript_count_file=transcript_count_file.sort_values(by=['tpm'], ascending=False)
-                    frist_row=transcript_count_file.iloc[:,0].unique()[:Max]
+                    # try to find column of counts
+                    transcript_count_file = transcript_count_file.sort_values(by=['tpm'], ascending=False)
+                    frist_row = transcript_count_file.iloc[:, 0].unique()[:Max]
                     print('Input matches kallisto output ')
 
                 except:
-                        pass
+                    pass
 
-
-
-
-                 #try to find row of counts
+                # try to find row of counts
                 try:
-                    #try to find row of counts
-                    transcript_count_file=transcript_count_file.sort_values(by=['counts'], ascending=False)
-                    frist_row=transcript_count_file.iloc[:,0].unique()[:Max]
+                    # try to find row of counts
+                    transcript_count_file = transcript_count_file.sort_values(by=['counts'], ascending=False)
+                    frist_row = transcript_count_file.iloc[:, 0].unique()[:Max]
                     print('Input with Counts column ')
 
                 except:
                     pass
 
-
-
                 # and let DIGGER do the magic ;)
-                job_num=str(random.randrange(500))
-                with open(f'{jobs_path}/{job_num}.txt', "wb") as fp:  pickle.dump(frist_row, fp)   #Pickling
-                return redirect(Multi_proteins,job=job_num)
+                job_num = str(random.randrange(500))
+                with open(f'{jobs_path}/{job_num}.txt', "wb") as fp:
+                    pickle.dump(frist_row, fp)  # Pickling
+                return redirect(Multi_proteins, job=job_num)
 
 
 
